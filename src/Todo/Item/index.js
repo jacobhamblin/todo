52fb20c212a78a778f8a1fc0f8cf0d32ee4766e8
Jacob Hamblin <jacob@hamblin.cc>
2018-12-29 13:53:32 -0700

    new approach

--- src/Todo/Item/index.js
+++ src/Todo/Item/index.js
@@ -1,33 +1,14 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './Item.css';
 
-class Item extends PureComponent {
-  onKeyUp = (event) => {
-    const { changeName, name, id } = this.props;
-    if (event.target.innerText !== name) {
-      changeName(id, event.target.innerText);
-    }
-  }
-  onClick = (event) => {
-    const { changeName, id, name } = this.props;
-    if (event.target.innerText !== name) changeName(id, event.target.innerText);
-  }
-  render() {
-    const { completed, id, name, toggleCompleted, } = this.props;
+const Item = ({id, name, toggleCompleted}) => {
     return (
       <li className='Item'>
-        <input type='checkbox' onClick={() => toggleCompleted(id)} value={completed} />
-        <div
-          ref={(node) => this.DOMItem = node}
-          contentEditable
-          onClick={this.onClick}
-          onKeyUp={this.onKeyUp}
-        >
+        <div onClick={() => {toggleCompleted(id)}}>
           {name}
         </div>
       </li>
     );
-  }
 }
 
 export default Item;
--- src/Todo/index.js
+++ src/Todo/index.js
@@ -5,7 +5,7 @@ import './Todo.css';
 class Todo extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {id: 2, items: {1: this.newItem(1)}};
+    this.state = {id: 1, showCompleted: false, newItemName: '', items: {}};
   }
   newItem = (id) => {
     return {id, completed: false, name: 'name'};
@@ -17,27 +17,60 @@ class Todo extends PureComponent {
   }
   toggleCompleted = (id) => {
     const { items } = this.state;
-    items[id].completed = !items[id].completed;
-    this.setState({ items });
+    const newItems = {...items}
+    newItems[id].completed = !newItems[id].completed;
+    this.setState({ items: newItems });
+  }
+  toggleShowCompleted = () => {
+    const { showCompleted } = this.state;
+    this.setState({showCompleted: !showCompleted});
   }
   changeName = (id, name) => {
     const { items } = this.state;
-    items[id].name = name;
-    this.setState({ items });
+    const newItems = {...items}
+    newItems[id].name = name;
+    this.setState({ items: newItems });
+  }
+  submitItem = () => {
+    const { id, items, newItemName } = this.state;
+    const newItems = {...items};
+    newItems[id] = {completed: false, id, name: newItemName};
+    this.setState({newItemName: '', items: newItems});
   }
   render() {
-    const props = { toggleCompleted: this.toggleCompleted, changeName: this.changeName, };
-    const { items } = this.state;
+    const props = {
+      addNewItem: this.addNewItem,
+      changeName: this.changeName,
+      toggleCompleted: this.toggleCompleted,
+    };
+    const { items, newItemName, showCompleted } = this.state;
+    const completedText = showCompleted ? 'Hide Completed' : 'Show Completed'
     return (
       <div className='Todo'>
         <h2>Todo</h2>
+        <div className='Todo__new-item'>
+          <input
+            className='Todo__new-item__name'
+            placeholder='What do you need to do?'
+            onChange={(e) => this.setState({newItemName: e.target.value})}
+            value={newItemName}
+          />
+          <button
+            className='Todo__new-item__button'
+            onClick={this.submitItem}
+          >
+            Add Todo Item
+          </button>
+        </div>
         <ul className='Todo__items-list'>
           {Object.keys(items).map(id => {
             const newProps = {...props, ...items[id], id, key: id};
             return <Item {...newProps} />
           })}
         </ul>
-        <a className='Todo__new-item' onClick={() => this.addNewItem()}>New Item</a>
+        <div className='Todo__buttons'>
+          <button className='Todo__show-completed' onClick={() => this.toggleShowCompleted()}>{completedText}</button>
+        </div>
       </div>
     )
   }
