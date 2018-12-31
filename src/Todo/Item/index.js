7c2e9f72a3cf4525cefcc521132c50bbb63c06a2
Jacob Hamblin <jacob@hamblin.cc>
2018-12-30 20:35:10 -0700

    different color when item completed

--- src/Todo/Item/index.js
+++ src/Todo/Item/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Item.css';
 
-const Item = ({id, name, toggleCompleted}) => {
-    return (
-      <li className='Item'>
-        <div onClick={() => {toggleCompleted(id)}}>
-          {name}
-        </div>
-      </li>
-    );
+const Item = ({id, completed, name, toggleCompleted}) => {
+  const style = completed ? {backgroundColor: '#00FF00'} : undefined;
+  return (
+    <li className='Item' style={style}>
+      <div onClick={() => {toggleCompleted(id)}}>
+        {name}
+      </div>
+    </li>
+  );
 }
 
 export default Item;
