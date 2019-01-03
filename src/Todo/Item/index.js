dc2ee60a112b566972ed3ed0ea73739136144413
Jacob Hamblin <jacob@hamblin.cc>
2019-01-02 19:41:00 -0700

    pastel colors

--- src/Todo/Item/index.js
+++ src/Todo/Item/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Item.css';
 
 const Item = ({id, completed, name, toggleCompleted}) => {
-  const style = completed ? {backgroundColor: '#00FF00'} : undefined;
+  const style = completed ? {backgroundColor: '#8BFEA8'} : undefined;
   return (
     <li className='Item' style={style} onClick={() => {toggleCompleted(id)}}>
       {name}
